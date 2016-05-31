d919a69919b76dfdb691119907af0832b3365873
Flemming Thobo-Carlsen <fle@fyp.dk>
2016-05-31 23:47:48 +0200

    WorkingWithInsertDataFrom WPF_part

--- AdminFormel1Wpf/MainWindow.xaml.cs
+++ AdminFormel1Wpf/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System.Windows;
+using Formel1.Database.Repository;
+using Formel1.Database.Entity;
+using System;
 
 namespace AdminFormel1Wpf
 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -10,6 +14,15 @@ namespace AdminFormel1Wpf
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnYearInsert_Click(object sender, RoutedEventArgs e)
+        {
+            var year = int.Parse(txtBoxYear.Text);
+            var time = new DateTime(year,1, 1);
+
+            var wpf = new WPF();
+            wpf.InsertSeason(new Season { Year = time });
         }
     }
 }
